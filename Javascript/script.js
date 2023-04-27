01f55f8990f4c54195041fc6e65595509a0f39f6
gohweichong <s10204647@connect.np.edu.sg>
2023-04-27 15:52:23 +0800

    Appended File locations

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,5 +1,5 @@
 let typingText = new Typed("#text", {
-    strings : ["Wei Chong", "Coder", "Adventurous"],
+    strings : ["Wei Chong", "a Coder", "Adventurous"],
     loop : true,
     typeSpeed : 100,
     backSpeed : 50,
